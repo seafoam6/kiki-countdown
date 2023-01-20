624c53e31638e5d9f2eae9cdc57353778a5a568c
Steve Barman <steve@stevebarman.com>
2023-01-19 20:12:47 -0500

    draft 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Countdown from "react-countdown";
+import "./App.css";
+
+const Completionist = () => <span>Times up!</span>;
+
+const renderer = ({ hours, minutes, seconds, completed, days }) => {
+  console.log({ hours, minutes, seconds, completed });
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {days}:{String(hours).padStart(2, 0)}:
+        {String(minutes + 1).padStart(2, 0)}
+      </span>
+    );
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Kiki Countdown ❤️ ❤️ ❤️ ❤️ ❤️
+        <Countdown date={"2023-02-04T00:44:00"} renderer={renderer} />
       </header>
     </div>
   );
